ac5ac1cca92a8956e9e2657047f8b85629e85c17
Aamirpal Sehmi <aamirpal.sehmi@gmail.com>
2024-01-14 05:13:26 -0700

    UPdate contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/aamirpalsehmi",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@aamirpalSehmi",
 	},
 	{
 		icon: <Mail size={20} />,
 		href: "mailto:dev@chronark.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "dev.aamirpal@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/aamirpal",
 		label: "Github",
-		handle: "chronark",
+		handle: "aamirpal",
 	},
 ];
 
