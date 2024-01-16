55f1f9a98748c7d4a58c2e982ce9d11a6f52a9a1
Aamirpal Sehmi <aamirpal.sehmi@gmail.com>
2024-01-15 23:02:31 -0700

    Update data

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "aamirpal.com",
+    template: "%s | aamirpal.com",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
+    title: "aamirpal.com",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    url: "https://aamirpal.com",
+    siteName: "aamirpal.com",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://aamirpal.com/og.png",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "Aamirpal",
     card: "summary_large_image",
   },
   icons: {
